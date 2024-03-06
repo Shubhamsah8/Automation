03befc011b602993e25ebde736ade70afd4848d1
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2024-03-06 10:13:17 +0530

    Add files via upload

---
+++ FacebookKeyAndMouse.java
@@ -0,0 +1,50 @@
+package Automation.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class FacebookKeyAndMouse {
+
+    public WebDriver driver;
+
+    @Test
+    public void login() throws AWTException {
+        driver = new ChromeDriver();
+
+        driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
+
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_S);
+        robot.keyRelease(KeyEvent.VK_S);
+        robot.keyPress(KeyEvent.VK_H);
+        robot.keyRelease(KeyEvent.VK_H);
+        robot.keyPress(KeyEvent.VK_U);
+        robot.keyRelease(KeyEvent.VK_U);
+        robot.keyPress(KeyEvent.VK_B);
+        robot.keyRelease(KeyEvent.VK_B);
+        robot.keyPress(KeyEvent.VK_H);
+        robot.keyRelease(KeyEvent.VK_H);
+        robot.keyPress(KeyEvent.VK_A);
+        robot.keyRelease(KeyEvent.VK_A);
+        robot.keyPress(KeyEvent.VK_M);
+        robot.keyRelease(KeyEvent.VK_M);
+
+        robot.keyPress(KeyEvent.VK_TAB);
+
+        robot.keyPress(KeyEvent.VK_S);
+        robot.keyRelease(KeyEvent.VK_S);
+        robot.keyPress(KeyEvent.VK_A);
+        robot.keyRelease(KeyEvent.VK_A);
+        robot.keyPress(KeyEvent.VK_H);
+        robot.keyRelease(KeyEvent.VK_H);
+
+        driver = new ChromeDriver();
+        
+
+    }
+}
