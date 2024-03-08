26b2b6b64d0ac582f13f81d45db801aa221ed972
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2024-03-08 09:02:37 +0530

    Add files via upload

--- PopUp/AutoIt/UploadFile.java
+++ PopUp/AutoIt/UploadFile.java
@@ -12,30 +12,20 @@ import java.io.IOException;
 import java.time.Duration;
 
 public class UploadFile {
+    public WebDriver driver;
     @Test
-    public void uploadFile(){
-        try {
-            // Execute AutoIt script to handle file selection
-            ProcessBuilder builder = new ProcessBuilder("\"C:\\Users\\RYZEN\\Downloads\\fileToUpload.exe\C:\Users\RYZEN\Downloads");
-            Process process = builder.start();
-            process.waitFor(); // Wait for the process to finish
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void uploadFile() throws IOException, InterruptedException {
+        driver = new ChromeDriver();
 
-        // Instantiate WebDriver after AutoIt script finishes executing
-        WebDriver driver = new ChromeDriver();
-
-        // Navigate to the webpage
         driver.get("https://smallpdf.com/compress-pdf");
+        driver.manage().window().maximize();
 
-        // Wait for the file upload button to become clickable
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement uploadButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='app']/div/div[1]/header/div[2]/div/div/label/form/label/div/div[2]/div[2]/button[1]/span")));
 
-        // Click the file upload button
+
+        WebElement uploadButton = driver.findElement(By.xpath("//button[@type='button']"));
         uploadButton.click();
-
+        Runtime.getRuntime().exec("\"C:\\Users\\RYZEN\\Downloads\\fileToUpload.exe\"");
+        Thread.sleep(5000);
 
     }
 }
